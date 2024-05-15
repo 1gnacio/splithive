654396cc26338b9c5b44f1e284775124c6bf9fe2
juanhosz <jhoszowski@fi.uba.ar>
2024-05-15 13:34:43 -0300

    US-13 Visualizar deudas en lista de contactos

--- src/utils/logicaNegocio.tsx
+++ src/utils/logicaNegocio.tsx
@@ -19,10 +19,38 @@ export default function calcularDeudas(integrantes,gastos){
     })
 
     return map
+}
 
+export function calcularDeudasAtravesDeGrupos(grupos,current_deudor){
+    var deuda_acumulada = 0;
+    var mapa_nombres_userid = new Map();
+    grupos.forEach(grupo =>{
+        grupo.integrantes.forEach(integrante =>{
+            mapa_nombres_userid.set(integrante.nombre,integrante.userID)
+        })
+    })
+    grupos.forEach(grupo =>{
+        grupo.gastos.forEach(gasto =>{
 
+            if (current_deudor== mapa_nombres_userid.get(gasto.payer)){
+                
+                deuda_acumulada -= gasto.deuda
+            }
+            else if (mapa_nombres_userid.get(gasto.payer) == 6){
+                gasto.deudores.forEach(deudor =>{
+
+                    if (!(mapa_nombres_userid.get(deudor) == 6)){
+                        if (mapa_nombres_userid.get(deudor) == current_deudor){
+                            deuda_acumulada += gasto.deuda
+                        }
+                    }
+    
+                })
+            }
+        })
+    })
+    console.log("Deuda acumulada es "+ deuda_acumulada)
+    return deuda_acumulada;
+}
 
         
-
-
-}
