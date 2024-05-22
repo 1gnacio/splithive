7d447fe09b239a82afc799e81b63ca8fdcaa6b40
Tomas-NO <tomasnolivera@gmail.com>
2024-05-22 14:09:17 -0300

    [US 12] Armado de grupo con no contacto - parte 1

--- src/utils/utilities.tsx
+++ src/utils/utilities.tsx
@@ -1,21 +1,33 @@
+export const getContactos =() =>{
+    return JSON.parse(sessionStorage.getItem("contactos"))
+}
 
-    export const getContactos =() =>{
-            return JSON.parse(sessionStorage.getItem("contactos"))
-    }
+export const getHives =() =>{
+    return JSON.parse(sessionStorage.getItem("hives"))
+}
 
-    export const getHives =() =>{
-        return JSON.parse(sessionStorage.getItem("hives"))
-    }
-    export const getUsuarios =() =>{
-        return JSON.parse(sessionStorage.getItem("usuarios"))
-    }
-    export const getGrupos =() =>{
-        return JSON.parse(sessionStorage.getItem("grupos"))
-    }
-    export const getGastos =() =>{
-        return JSON.parse(sessionStorage.getItem("gastos"))
-    }
+export const getUsuarios =() =>{
+    return JSON.parse(sessionStorage.getItem("usuarios"))
+}
 
-    export const getCurrentUser = () =>{
-        return JSON.parse(sessionStorage.getItem("userID"))
+export const getGrupos =() =>{
+    return JSON.parse(sessionStorage.getItem("grupos"))
+}
+
+export const getGastos =() =>{
+    return JSON.parse(sessionStorage.getItem("gastos"))
+}
+
+export const getCurrentUser = () =>{
+    return JSON.parse(sessionStorage.getItem("userID"))
+}
+
+export const getUserByUsername = (username) => {
+    const usuarios = getUsuarios();
+    for(const id in usuarios) {
+        if(usuarios[id].usuario === username) {
+            return usuarios[id];
+        }
     }
+    return null;
+}
