7c62c73b9030fef29dc129c0738bcb1fb15e1215
Cami <82735635+c-gonzalez-a@users.noreply.github.com>
2024-05-23 18:20:58 -0300

    Refactor header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="header">
+    <img src='/splithive.png' alt="Bees" className="img-logo" />
+  </div>
+);
+
+export default Header;
